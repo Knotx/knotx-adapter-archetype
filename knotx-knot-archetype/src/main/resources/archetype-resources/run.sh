45ca9692e0efed123af6f0784f27c73c701801a3
Maciej Laskowski <maciej.laskowski@cognifide.com>
2018-10-09 11:05:56 +0200

    update knot archetype to 1.3

--- knotx-knot-archetype/src/main/resources/archetype-resources/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Dlogback.configurationFile=knotx-standalone.logback.xml -cp "app/*" io.knotx.launcher.LogbackLauncher -conf knotx-standalone.json
---
+++ knotx-knot-archetype/src/main/resources/archetype-resources/src/main/java/knot/example/package-info.java
@@ -0,0 +1,4 @@
+@ModuleGen(name = "${projectName}", groupPackage = "${package}")
+package ${package}.knot.example;
+
+import io.vertx.codegen.annotations.ModuleGen;
